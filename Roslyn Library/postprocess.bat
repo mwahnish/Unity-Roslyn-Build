5a0e3699744f8205412fd10bfe280937c654421e
Mark Wahnish <mark@abxygames.com>
2019-09-25 12:25:34 -0400

    Added postprocessing to build unity project

--- Roslyn Library/postprocess.bat
+++ Roslyn Library/postprocess.bat
@@ -17,3 +17,6 @@ del bin\Release\netstandard2.0\System.Reflection.Emit.dll
 del bin\Release\netstandard2.0\System.Reflection.Emit.ILGeneration.dll
 del bin\Release\netstandard2.0\System.Reflection.Emit.Lightweight.dll
 del bin\Release\netstandard2.0\System.Threading.dll
+copy "Unity-artifacts\License.txt" "bin\Release\netstandard2.0\License.txt"
+copy "Unity-artifacts\package.json" "bin\Release\netstandard2.0\package.json"
+copy "Unity-artifacts\Roslyn Assembly.asmdef" "bin\Release\netstandard2.0\Roslyn Assembly.asmdef"
